1d800b72485a942bec50b17a45172984470dcea7
malelew@ucsd.edu <malelew@ucsd.edu>
2018-09-01 18:31:46 -0500

    working waybackpack call -- issues with date range

--- get_follower_count.py
+++ get_follower_count.py
@@ -1,31 +1,31 @@
 import sys
 import subprocess
 import pandas
-from bs4 import BeautifulSoup
 import requests
 import logging
+from bs4 import BeautifulSoup
 
 twitter_url = "https://twitter.com/"
-arg_num = 3
+arg_num = 4
 
-#if (len(argv) != arg_num):
-#    return "Incorrect number of arugments"
+if (len(sys.argv) != arg_num):
+    raise ValueError("Incorrect number of arguemts please pass three arugments: twitte handle, from_date formatted as MMDDYYY, to_date formatted as MMDDYYY")
 
-twitter_handle = str(sys.argv[0])
-from_date = str(sys.argv[1])
-to_date = str(sys.argv[2])
+twitter_handle = str(sys.argv[1])
+from_date = str(sys.argv[2])
+to_date = str(sys.argv[3])
 
 twitter_handle_url = twitter_url + twitter_handle
 
 # waybackpack pull arugments
-directory_arg = "-d " + twitter_handle + " "# where to save the archived pages
+directory_arg = "-d ./" + twitter_handle + " "# where to save the archived pages
 raw_arg = "--raw " # fetch archives in their original state
 from_date_arg = "--from-date " + from_date + " " # first archive to pull
-to_date_arg = "--to-date" + to_date + " " # last archive to pull
+to_date_arg = "--to-date " + to_date + " " # last archive to pull
 follow_redirects_arg = "--follow-redirects " # follow redirects
 uniques_only_arg = "--uniques-only " # Download only the first version of duplicate filesself.
 quiet_arg = "--quiet " # Don't log progress to stderr. improves run time
 
-subprocess.call("waybackpack " + directory_arg + raw_arg + from_date_arg +
-                 to_date_arg + follow_redirects_arg + uniques_only_arg +
-                 quiet_arg + twitter_handle_url)
+waybackpack_call = "waybackpack " + directory_arg + raw_arg + from_date_arg + to_date_arg + follow_redirects_arg + uniques_only_arg + quiet_arg + twitter_handle_url
+
+subprocess.call(waybackpack_call, shell=True)
